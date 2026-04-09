16521f7f6384c34504e2da3f1df625205b6701aa
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2026-04-09 10:01:53 +0200

    Add tests

--- src/test/java/de/swdec/jaccount/keycloak/spi/extensions/OrganizationsGroupsStructuredMapperTest.java
+++ src/test/java/de/swdec/jaccount/keycloak/spi/extensions/OrganizationsGroupsStructuredMapperTest.java
@@ -7,8 +7,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import org.keycloak.models.*;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.GroupModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.OrganizationModel;
 import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.UserSessionModel;
 import org.keycloak.organization.OrganizationProvider;
 import org.keycloak.representations.AccessToken;
 
@@ -16,10 +21,167 @@ public class OrganizationsGroupsStructuredMapperTest {
 
     @Test
     void shouldAddOrganizationsAsGroups() {
-        OrganizationsGroupsStructuredMapper mapper =
-            new OrganizationsGroupsStructuredMapper();
+        OrganizationsGroupsStructuredMapper mapper = new OrganizationsGroupsStructuredMapper();
 
-        // ----- mocks -----
+        KeycloakSession session = mock(KeycloakSession.class);
+        OrganizationProvider orgProvider = mock(OrganizationProvider.class);
+        UserSessionModel userSession = mock(UserSessionModel.class);
+        UserModel user = mock(UserModel.class);
+        ProtocolMapperModel mappingModel = new ProtocolMapperModel();
+        OrganizationModel org = mock(OrganizationModel.class);
+
+        when(session.getProvider(OrganizationProvider.class)).thenReturn(orgProvider);
+        when(userSession.getUser()).thenReturn(user);
+        when(orgProvider.getByMember(user)).thenReturn(Stream.of(org));
+        when(org.isEnabled()).thenReturn(true);
+        when(org.getId()).thenReturn("org1");
+        when(org.getName()).thenReturn("Acme");
+        when(org.getAttributes()).thenReturn(Map.of());
+        when(orgProvider.getOrganizationGroupsByMember(org, user)).thenReturn(Stream.empty());
+
+        mappingModel.setConfig(
+                Map.of(
+                        "claim.name", "groups",
+                        "orgs_as_groups", "true",
+                        "org_id_prefix", "org:",
+                        "group_id_prefix", ""));
+
+        AccessToken token = new AccessToken();
+
+        mapper.transformUserInfoToken(
+                token,
+                mappingModel,
+                session,
+                userSession,
+                mock(ClientSessionContext.class));
+
+        Object claim = token.getOtherClaims().get("groups");
+        assertNotNull(claim);
+
+        List<?> groups = (List<?>) claim;
+        assertEquals(1, groups.size());
+
+        Map<?, ?> first = (Map<?, ?>) groups.get(0);
+        assertEquals("org:org1", first.get("gid"));
+        assertEquals("Acme", first.get("displayName"));
+    }
+
+    @Test
+    void shouldAddOrganizationGroupsWithPrefixAndPrependOrganizationName() {
+        OrganizationsGroupsStructuredMapper mapper = new OrganizationsGroupsStructuredMapper();
+
+        KeycloakSession session = mock(KeycloakSession.class);
+        OrganizationProvider orgProvider = mock(OrganizationProvider.class);
+        UserSessionModel userSession = mock(UserSessionModel.class);
+        UserModel user = mock(UserModel.class);
+        ProtocolMapperModel mappingModel = new ProtocolMapperModel();
+
+        OrganizationModel org = mock(OrganizationModel.class);
+        GroupModel group = mock(GroupModel.class);
+
+        when(session.getProvider(OrganizationProvider.class)).thenReturn(orgProvider);
+        when(userSession.getUser()).thenReturn(user);
+        when(orgProvider.getByMember(user)).thenReturn(Stream.of(org));
+        when(org.isEnabled()).thenReturn(true);
+        when(org.getId()).thenReturn("org1");
+        when(org.getName()).thenReturn("Acme");
+        when(org.getAttributes()).thenReturn(Map.of());
+
+        when(group.getId()).thenReturn("group1");
+        when(group.getName()).thenReturn("Group 1");
+        when(group.getAttributes()).thenReturn(Map.of());
+        when(orgProvider.getOrganizationGroupsByMember(org, user)).thenReturn(Stream.of(group));
+        when(orgProvider.getByMember(user)).thenReturn(Stream.of(org));
+
+        mappingModel.setConfig(
+                Map.of(
+                        "claim.name", "groups",
+                        "orgs_as_groups", "false",
+                        "group_id_prefix", "grp:",
+                        "prepend_org_to_group_name", "true"));
+
+        AccessToken token = new AccessToken();
+
+        mapper.transformUserInfoToken(
+                token,
+                mappingModel,
+                session,
+                userSession,
+                mock(ClientSessionContext.class));
+
+        Object claim = token.getOtherClaims().get("groups");
+        assertNotNull(claim);
+
+        List<?> groups = (List<?>) claim;
+        assertEquals(1, groups.size());
+
+        Map<?, ?> first = (Map<?, ?>) groups.get(0);
+        assertEquals("grp:group1", first.get("gid"));
+        assertEquals("Acme/Group 1", first.get("displayName"));
+    }
+
+    @Test
+    void shouldOverrideIdsFromAttributesForOrganizationsAndGroups() {
+        OrganizationsGroupsStructuredMapper mapper = new OrganizationsGroupsStructuredMapper();
+
+        KeycloakSession session = mock(KeycloakSession.class);
+        OrganizationProvider orgProvider = mock(OrganizationProvider.class);
+        UserSessionModel userSession = mock(UserSessionModel.class);
+        UserModel user = mock(UserModel.class);
+        ProtocolMapperModel mappingModel = new ProtocolMapperModel();
+
+        OrganizationModel org = mock(OrganizationModel.class);
+        GroupModel group = mock(GroupModel.class);
+
+        when(session.getProvider(OrganizationProvider.class)).thenReturn(orgProvider);
+        when(userSession.getUser()).thenReturn(user);
+        when(orgProvider.getByMember(user)).thenReturn(Stream.of(org));
+        when(org.isEnabled()).thenReturn(true);
+        when(org.getId()).thenReturn("org1");
+        when(org.getName()).thenReturn("Acme");
+        when(org.getAttributes()).thenReturn(Map.of("legacyId", List.of("legacy-org-id")));
+
+        when(group.getId()).thenReturn("group1");
+        when(group.getName()).thenReturn("Group 1");
+        when(group.getAttributes()).thenReturn(Map.of("legacyId", List.of("legacy-group-id")));
+        when(orgProvider.getOrganizationGroupsByMember(org, user)).thenReturn(Stream.of(group));
+
+        mappingModel.setConfig(
+                Map.of(
+                        "claim.name", "groups",
+                        "orgs_as_groups", "true",
+                        "org_id_prefix", "org:",
+                        "group_id_prefix", "grp:",
+                        "id_override_attr", "legacyId"));
+
+        AccessToken token = new AccessToken();
+
+        mapper.transformUserInfoToken(
+                token,
+                mappingModel,
+                session,
+                userSession,
+                mock(ClientSessionContext.class));
+
+        Object claim = token.getOtherClaims().get("groups");
+        assertNotNull(claim);
+
+        List<?> groups = (List<?>) claim;
+        assertEquals(2, groups.size());
+
+        Map<?, ?> orgEntry = (Map<?, ?>) groups.get(0);
+        Map<?, ?> groupEntry = (Map<?, ?>) groups.get(1);
+
+        assertEquals("legacy-org-id", orgEntry.get("gid"));
+        assertEquals("Acme", orgEntry.get("displayName"));
+
+        assertEquals("legacy-group-id", groupEntry.get("gid"));
+        assertEquals("/Group 1", groupEntry.get("displayName"));
+    }
+
+    @Test
+    void shouldSkipDisabledOrganizations() {
+        OrganizationsGroupsStructuredMapper mapper = new OrganizationsGroupsStructuredMapper();
 
         KeycloakSession session = mock(KeycloakSession.class);
         OrganizationProvider orgProvider = mock(OrganizationProvider.class);
@@ -29,58 +191,22 @@ public class OrganizationsGroupsStructuredMapperTest {
 
         OrganizationModel org = mock(OrganizationModel.class);
 
-        when(session.getProvider(OrganizationProvider.class)).thenReturn(
-            orgProvider
-        );
-
+        when(session.getProvider(OrganizationProvider.class)).thenReturn(orgProvider);
         when(userSession.getUser()).thenReturn(user);
-
         when(orgProvider.getByMember(user)).thenReturn(Stream.of(org));
+        when(org.isEnabled()).thenReturn(false);
 
-        when(org.getId()).thenReturn("org1");
-        when(org.getName()).thenReturn("Acme");
-        when(org.isEnabled()).thenReturn(true);
-
-        // ----- mapper config -----
-
-        mappingModel.setConfig(
-            Map.of(
-                "claim.name",
-                "groups",
-                "orgs_as_groups",
-                "true",
-                "org_id_prefix",
-                "org:",
-                "group_id_prefix",
-                ""
-            )
-        );
+        mappingModel.setConfig(Map.of("claim.name", "groups"));
 
         AccessToken token = new AccessToken();
 
-        // ----- execute -----
-
         mapper.transformUserInfoToken(
-            token,
-            mappingModel,
-            session,
-            userSession,
-            mock(ClientSessionContext.class)
-        );
+                token,
+                mappingModel,
+                session,
+                userSession,
+                mock(ClientSessionContext.class));
 
-        // ----- assertions -----
-
-        Object claim = token.getOtherClaims().get("groups");
-
-        assertNotNull(claim);
-
-        List<?> groups = (List<?>) claim;
-
-        assertEquals(1, groups.size());
-
-        Map<?, ?> first = (Map<?, ?>) groups.get(0);
-
-        assertEquals("org:org1", first.get("gid"));
-        assertEquals("Acme", first.get("displayName"));
+        assertNull(token.getOtherClaims().get("groups"));
     }
 }
